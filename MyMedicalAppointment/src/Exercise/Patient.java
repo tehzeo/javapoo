f69becc1a96ec5fa066e474bd8940dc25b53b3a8
Teh Zeo <86634981+tehzeo@users.noreply.github.com>
2021-07-02 16:43:47 -0700

    1.0 uploading repository files!

---
+++ MyMedicalAppointment/src/Exercise/Patient.java
@@ -0,0 +1,14 @@
+package Exercise;
+
+public class Patient {
+
+    private String name;
+    private String email;
+    private String birthday;
+    private double weight;
+    private double height;
+    private String blood;
+
+    Patient(String name, String email){
+        this.name = name;
+        this.email = email;
